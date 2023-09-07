02bbe1ec4cf9e5b9dfe6df0f5ec1537e2bc486d4
alexanderivv <thealexashka@gmail.com>
2023-09-07 17:33:13 +0300

    activity indicator added

--- MarketApp/CustomCells/ProductTableViewCell/ProductTableViewCell.swift
+++ MarketApp/CustomCells/ProductTableViewCell/ProductTableViewCell.swift
@@ -12,9 +12,24 @@ class ProductTableViewCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var productImageView: UIImageView!
-
+    
+    var activityIndicator: UIActivityIndicatorView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupActivityIndicator()
+    }
+    
     func configure(with product: Product) {
         nameLabel.text = product.title
+        descriptionLabel.text = product.description
+        categoryLabel.text = product.category
+        
+        productImageView.image = nil
+        
+        if let activityIndicator = activityIndicator {
+            activityIndicator.startAnimating()
+        }
         
         APIManager.shared.fetchImage(urlString: product.image) { [weak self] result in
             switch result {
@@ -22,14 +37,21 @@ class ProductTableViewCell: UITableViewCell {
                 DispatchQueue.main.async {
                     if let self = self, product.title == self.nameLabel.text {
                         self.productImageView.image = image
+                        self.activityIndicator?.stopAnimating()
                     }
                 }
             case .failure(let error):
                 print("Error fetching image: \(error)")
+                self?.activityIndicator?.stopAnimating()
             }
         }
-        
-        descriptionLabel.text = product.description
-        categoryLabel.text = product.category
+    }
+    
+    private func setupActivityIndicator() {
+        let indicator = UIActivityIndicatorView(style: .medium)
+        indicator.hidesWhenStopped = true
+        addSubview(indicator)
+        indicator.center = CGPoint(x: bounds.midX, y: bounds.midY)
+        activityIndicator = indicator
     }
 }
